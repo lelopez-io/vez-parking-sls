96a7ea8d4fcb8635b469ff5eb7211d2de4209fdc
Luis E. Lopez <35977981+lelopez-io@users.noreply.github.com>
2019-08-02 13:42:06 -0500

    Added comments

--- UpdateSensorData/index.ts
+++ UpdateSensorData/index.ts
@@ -1,11 +1,37 @@
 import { AzureFunction, Context } from "@azure/functions"
 
+import { CosmosDB } from "../SharedCode/CosmosDB";
+
 const IoTHubTrigger: AzureFunction = async function (context: Context, IoTHubMessages: any[]): Promise<void> {
     context.log(`Eventhub trigger function called for message array: ${IoTHubMessages}`);
     
     IoTHubMessages.forEach(message => {
         context.log(`Processed message: ${message}`);
+        
+        // TODO: Read the actual values of the message and create another object
+
+        // eventData will be a structure Typescript calls them interfaces
+        // messages will mostly likely be an array of eventData
+        // this interface file will be in SharedCode since it also get's checked
+        // on the iot device
+
     });
+
+    // TODO: once you can read the eventData we need to save that to the DB
+    // 
+
+    let databaseHandle = new CosmosDB();
+    
+    // these are only used once can remove them later
+    await databaseHandle.CreateDatabase("vez");
+    await databaseHandle.CreateCollection("test-parkinglot")
+    // above two will be removed after
+
+    // Once a database exists we can connect to it and start adding/updating
+    // the parking spots
+    await databaseHandle.SetConnectionTo("vez", "test-parkinglot");
+
+
 };
 
 export default IoTHubTrigger;
